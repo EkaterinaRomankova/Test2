5afaaede0a2ceb62cc7149b084fc163925195d5a
EkaterinaRomankova <catia.romankova@yandex.by>
2022-10-11 21:17:00 +0300

    hhh commit

---
+++ g.js
@@ -0,0 +1,64 @@
+// console.log("Hello");
+// console.log("hello monkey")
+// console.log("hello money")
+// console.log("hello baby")
+
+// for(let i = 0; i <= 3; i++){
+//     console.log(i)
+// }
+
+// let str = 'Hello';
+// let str1 = '';
+// for(let i = 1; i <= 7; i++){
+//     str1 = str1 + str;
+//     console.log(str1);
+// }
+// console.log(str1)
+
+// let sum = 0;
+// for(let i = 1; i <= 10; i++){
+//     sum = sum + i;
+//     console.log(sum);
+// }
+// console.log(sum);
+
+// let str = 'JavaScript';
+// for(let i = 0; i < str.length; i++){
+//     console.log(str[i])
+// }
+
+
+// let x = [1, 2, 3, 4, 5];
+// let str2 = '';
+// for(let i = x.length - 1; i >= 0; i--){
+//     str2 = str2 + x[i];
+//     console.log(str2);
+// } 
+// console.log(str2)
+
+// let c = [24, 56, 78, 99];
+// for(let x in c){
+//     c[x] 
+//     console.log(c)
+// }
+// console.log(c)
+
+// let cars = ["BMW", "Volovo", "Mers"];
+// let text = '';
+// for(let x of cars){
+//     text += x + ' ';
+// }
+// console.log(text)
+
+
+
+
+
+
+
+
+
+
+
+
+
